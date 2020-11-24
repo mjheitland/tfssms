0a8725575b02d02c95f93760db9099a6de25ef4b
Michael Heitland <heitlm@amazon.de>
2020-11-24 07:55:50 +0100

    Update outputs.tf

--- compute/outputs.tf
+++ compute/outputs.tf
@@ -1,9 +1,9 @@
 #--- compute/outputs.tf
 output "keypair_id" {
-  value = "${join(", ", aws_key_pair.tfmh_keypair.*.id)}"
+  value = join(", ", aws_key_pair.tfmh_keypair.*.id)
 }
 output "server_ids" {
-  value = "${join(", ", aws_instance.tfmh_server.*.id)}"
+  value = join(", ", aws_instance.tfmh_server.*.id)
 }
 output "server_public_ips" {
   value = join(", ", aws_instance.tfmh_server.*.public_ip)
